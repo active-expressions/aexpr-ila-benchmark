c5b2ba68cec3b71ef996220d7f7de7270b4aff53
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-20 14:57:26 +0200

    interpretation bundling

--- benchmark/tests/interpretation.js
+++ benchmark/tests/interpretation.js
@@ -1,48 +1,26 @@
 import perfTest from '../perf_test.js';
+import rand from 'random-seed';
+import { createRectangle } from './fixture.js';
 
 import { aexprInterpretation } from 'active-expressions';
-import rand from 'random-seed';
-
-console.log(rand);
-
-var foo = rand.create(42);
-var bar = rand.create(42);
-for(let i = 0; i < 1001; i++) {
-  console.log(foo.random() === bar.random());
-}
 
 describe('Interpretation Benchmarks', function() {
   this.timeout("2000s");
 
   describe("Run", function() {
 
-    function area() {
-      return this.width * this.height;
-    }
+    let aspectRatioCount = 1000;
+    const targetAspectRatio = 2;
+    let aspectRatioRand = rand.create('aspectRatio');
 
-    beforeEach(() => {
-      console.log(arguments);
-    });
+    it("Maintain Aspect Ratio", perfTest(function () {
+      let rect = createRectangle(20, 10);
+      aexprInterpretation(() => rect.aspectRatio(), {rect})
+          .onChange(ratio => rect.height = rect.width / targetAspectRatio);
 
-    it("Changing Rectangles", perfTest(function () {
-      var count = 10;
-
-      var rects = [];
-      for(let i = 0; i < count; i++) {
-        rects.push({
-          width: i,
-          height: 10,
-          area
-        });
-      }
-
-      for(let i = 0; i < count; i++) {
-        aexprInterpretation(function() { return rects[i].area() }, {rects, i});
-      }
-
-      for(let i = 0; i < count; i++) {
-        rects[i].width += 1;
-        expect(rects[i].area()).to.be.greaterThan(0);
+      for(let i = 0; i < aspectRatioCount; i++) {
+        rect.width = aspectRatioRand.random();
+        expect(rect.aspectRatio()).to.equal(targetAspectRatio);
       }
     }));
   });
---
+++ rollup.config.interpretation.js
@@ -0,0 +1,15 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+    entry: 'benchmark/tests/interpretation.js',
+    dest: 'benchmark/temp/interpretation/interpretation_babel_in.js',
+    plugins: [
+        nodeResolve({
+            jsnext: true,
+            main: true,
+            skip: [ 'active-expressions' ]
+        }),
+        commonjs()
+    ]
+};
