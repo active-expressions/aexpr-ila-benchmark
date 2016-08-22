58dc0da9ac593e837bb2550eda48bb2d07585cb7
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-22 17:05:38 +0200

    added benchmark for partially rewritten code base

--- benchmark/config/rollup.config.rewrite1.js
+++ benchmark/config/rollup.config.rewrite1.js
@@ -1,9 +1,22 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import * as path from 'path';
 
 export default {
     entry: 'benchmark/tests/rewriting.js',
     dest: 'benchmark/temp/rewriting/rewriting_aexpr.js',
+    external: [
+        path.resolve('benchmark/tests/deps/toSkip0.js'),
+        path.resolve('benchmark/tests/deps/toSkip1.js'),
+        path.resolve('benchmark/tests/deps/toSkip2.js'),
+        path.resolve('benchmark/tests/deps/toSkip3.js'),
+        path.resolve('benchmark/tests/deps/toSkip4.js'),
+        path.resolve('benchmark/tests/deps/toSkip5.js'),
+        path.resolve('benchmark/tests/deps/toSkip6.js'),
+        path.resolve('benchmark/tests/deps/toSkip7.js'),
+        path.resolve('benchmark/tests/deps/toSkip8.js'),
+        path.resolve('benchmark/tests/deps/toSkip9.js')
+    ],
     plugins: [
         nodeResolve({
             jsnext: true,
--- benchmark/tests/rewriting.js
+++ benchmark/tests/rewriting.js
@@ -4,6 +4,28 @@ import { createRectangle } from './fixture.js';
 
 import quickSort from './deps/quicksort.js';
 
+import rewrittenCalc1 from './deps/toRewrite1.js';
+import rewrittenCalc2 from './deps/toRewrite2.js';
+import rewrittenCalc3 from './deps/toRewrite3.js';
+import rewrittenCalc4 from './deps/toRewrite4.js';
+import rewrittenCalc5 from './deps/toRewrite5.js';
+import rewrittenCalc6 from './deps/toRewrite6.js';
+import rewrittenCalc7 from './deps/toRewrite7.js';
+import rewrittenCalc8 from './deps/toRewrite8.js';
+import rewrittenCalc9 from './deps/toRewrite9.js';
+import rewrittenCalc0 from './deps/toRewrite0.js';
+
+import plainCalc1 from './deps/toSkip1.js';
+import plainCalc2 from './deps/toSkip2.js';
+import plainCalc3 from './deps/toSkip3.js';
+import plainCalc4 from './deps/toSkip4.js';
+import plainCalc5 from './deps/toSkip5.js';
+import plainCalc6 from './deps/toSkip6.js';
+import plainCalc7 from './deps/toSkip7.js';
+import plainCalc8 from './deps/toSkip8.js';
+import plainCalc9 from './deps/toSkip9.js';
+import plainCalc0 from './deps/toSkip0.js';
+
 describe('Rewriting Benchmarks', function() {
   this.timeout("2000s");
 
@@ -42,5 +64,38 @@ describe('Rewriting Benchmarks', function() {
         quickSort(items);
       }
     }));
+  });
+
+  describe("Partially Rewritten", function() {
+
+    for(let i = 0; i <= 10; i++) {
+      let calculations = [];
+      let items;
+
+      it(`Partially Rewritten (${i} of 10)`, perfTest({
+        setupRun() {
+          let quickSortRand = rand.create('partiallyRewritten');
+
+          calculations[0] = i > 0 ? rewrittenCalc0 : plainCalc0;
+          calculations[1] = i > 1 ? rewrittenCalc1 : plainCalc1;
+          calculations[2] = i > 2 ? rewrittenCalc2 : plainCalc2;
+          calculations[3] = i > 3 ? rewrittenCalc3 : plainCalc3;
+          calculations[4] = i > 4 ? rewrittenCalc4 : plainCalc4;
+          calculations[5] = i > 5 ? rewrittenCalc5 : plainCalc5;
+          calculations[6] = i > 6 ? rewrittenCalc6 : plainCalc6;
+          calculations[7] = i > 7 ? rewrittenCalc7 : plainCalc7;
+          calculations[8] = i > 8 ? rewrittenCalc8 : plainCalc8;
+          calculations[9] = i > 9 ? rewrittenCalc9 : plainCalc9;
+
+          items = [];
+          for(let j = 0; j < 30; j++) { // 50
+            items.push(quickSortRand.random());
+          }
+        },
+        run() {
+          calculations.forEach(calc => calc(items));
+        }
+      }));
+    }
   });
 });
