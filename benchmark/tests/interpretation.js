2bb964f9a59f48a0b62a10c46dd2ca192c6ddbd6
Stefan Lehmann <stefan.lehmann@hpi.de>
2016-08-23 14:32:39 +0200

    added growing portion of wrapped (interpreted) instances

--- benchmark/tests/interpretation.js
+++ benchmark/tests/interpretation.js
@@ -24,4 +24,36 @@ describe('Interpretation Benchmarks', function() {
       }
     }));
   });
+
+  describe("Run", function() {
+
+    let rects;
+
+    for(let i = 0; i <= 10; i++) {
+      it(`Partially wrapped (${i} of 10)`, perfTest({
+        setupRun() {
+          rects = [];
+          for(let j = 0; j < 10; j++) {
+            let rect = createRectangle(20, 10);
+
+            if(j < i) {
+              console.log(`wrap rect (${j})`);
+              aexprInterpretation(() => rect.aspectRatio(), {rect});
+            }
+
+            rects[j] = rect;
+          }
+        },
+        run() {
+          for (let i = 0; i < 10000; i++) {
+            for (let l = 0; l < 10; l++) {
+              let r = rects[l];
+              r.height -= r.width;
+              r.width += r.height;
+            }
+          }
+        }
+      }));
+    }
+  });
 });
