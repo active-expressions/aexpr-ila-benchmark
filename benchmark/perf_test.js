264e030e5e08ac71fea1da04ea242a9e547cc885
Stefan Lehmann <stefan.lehmann@hpi.de>
2017-02-19 00:13:31 +0100

    split benchmarks into separate test cases

--- benchmark/perf_test.js
+++ benchmark/perf_test.js
@@ -1,48 +1,25 @@
 import { times } from './tests/test-utils.js';
 
-function standardDeviation(values){
-  var avg = average(values);
-
-  var squareDiffs = values.map(function(value){
-    var diff = value - avg;
-    return diff * diff;
-  });
-
-  var avgSquareDiff = average(squareDiffs);
-
-  return Math.sqrt(avgSquareDiff);
-}
-
-function average(data){
-  var sum = data.reduce(function(sum, value){
-    return sum + value;
-  }, 0);
-
-  return sum / data.length;
-}
-
 function performSingleRun(runner) {
-  runner.setupRun && runner.setupRun();
+    runner.setupRun && runner.setupRun();
 
-  var start = performance.now();
-  runner.run.call(this);
-  var duration = (performance.now() - start);
+    var start = performance.now();
+    runner.run.call(this);
+    var duration = (performance.now() - start);
 
-  runner.teardownRun && runner.teardownRun();
+    runner.teardownRun && runner.teardownRun();
 
-  return duration;
+    return duration;
 }
 
 export default function perfTest(it, name, runner) {
-  if(runner instanceof Function) {
-    runner = {
-      run: runner
-    };
-  }
+    if(runner instanceof Function) {
+        runner = {
+            run: runner
+        };
+    }
 
-  const maxRuns = 100,
-      slidingWindowSize = 30,
-      allowPercentagedDeviation = 4;
+    const maxRuns = 100;
 
     times(maxRuns, i => {
         it(name, function () {
@@ -57,35 +34,4 @@ export default function perfTest(it, name, runner) {
             return duration;
         });
     });
-
-  return;
-
-  it(name, function () {
-    let results = [];
-
-    for(var i = 0; i < maxRuns; i++) {
-        results.push(performSingleRun(runner));
-
-        // if(results.length >= slidingWindowSize) {
-        //   let time = average(results.slice(-slidingWindowSize));
-        //   let percentagedStandardDeviation = 100 * standardDeviation(results.slice(-slidingWindowSize)) / time;
-        //   if(percentagedStandardDeviation <= allowPercentagedDeviation) {
-        //     break;
-        //   }
-        // }
-    }
-
-    let lastMeasurements = results.slice(-slidingWindowSize),
-        time = average(lastMeasurements),
-        deviation = 100 * standardDeviation(lastMeasurements) / time;
-
-    this._runnable.title = JSON.stringify({
-        name: this._runnable.title,
-        average: time.toFixed(3),
-        standardDeviation: deviation.toFixed(1),
-        results: results
-    });
-
-    return time;
-  });
 };
--- karma.conf.js
+++ karma.conf.js
@@ -4,119 +4,188 @@ var fs = require('fs');
 var builder = require('xmlbuilder');
 var _ = require('lodash');
 
+function standardDeviation(values){
+    var avg = average(values);
+
+    var squareDiffs = values.map(function(value){
+        var diff = value - avg;
+        return diff * diff;
+    });
+
+    var avgSquareDiff = average(squareDiffs);
+
+    return Math.sqrt(avgSquareDiff);
+}
+
+function average(data){
+    var sum = data.reduce(function(sum, value){
+        return sum + value;
+    }, 0);
+
+    return sum / data.length;
+}
+
 // TODO: extract reporter to its own file
 var JSONReporter = function(baseReporterDecorator, config, emitter, logger, helper, formatError) {
-  var json;
+    var json;
 
     var outputFile = config.jsonReporter.outputFile;
-  var log = logger.create('reporter.json');
+    var log = logger.create('reporter.json');
 
-  var pendingFileWritings = 0;
-  var fileWritingFinished = function() {};
+    var pendingFileWritings = 0;
+    var fileWritingFinished = function() {};
 
-  baseReporterDecorator(this);
+    baseReporterDecorator(this);
 
-  // TODO: remove if public version of this method is available
-  var basePathResolve = function(relativePath) {
+    // TODO: remove if public version of this method is available
+    var basePathResolve = function(relativePath) {
 
-    if (helper.isUrlAbsolute(relativePath)) {
-      return relativePath;
-    }
+        if (helper.isUrlAbsolute(relativePath)) {
+            return relativePath;
+        }
 
-    if (!helper.isDefined(config.basePath) || !helper.isDefined(relativePath)) {
-      return '';
-    }
+        if (!helper.isDefined(config.basePath) || !helper.isDefined(relativePath)) {
+            return '';
+        }
 
-    return path.resolve(config.basePath, relativePath);
-  };
-
-  this.onRunStart = function(browsers) {
-    json = {};
-  };
-
-  this.onBrowserStart = function(browser) {
-    json[browser.id] = {
-        browser: browser.name,
-        suites: [],
-        partials: []
+        return path.resolve(config.basePath, relativePath);
     };
-  };
 
-  this.onBrowserError = function(browser, error) {
-    console.error('SOMETHING TERRIBLE HAPPENED IN: ' + browser.name, error);
-  };
+    this.onRunStart = function(browsers) {
+        json = {};
+    };
 
-  this.onBrowserComplete = function(browser) {
-      var result = browser.lastResult;
-      var bro = json[browser.id];
-      bro.test = {
-          tests: result.total + ' tests',
-          errors: (result.disconnected || result.error ? 1 : 0) + ' errors',
-          failed: result.failed + ' failures',
-          skipped: result.skipped + ' skipped',
-          runtime: ((result.netTime || 0) / 1000) + 's'
-      };
-  };
+    this.onBrowserStart = function(browser) {
+        json[browser.id] = {
+            browser: browser.name,
+            suites: [],
+            partials: []
+        };
+    };
 
-  this.onRunComplete = function(browsers) {
-    var jsonToOutput = json;
+    this.onBrowserError = function(browser, error) {
+        console.error('SOMETHING TERRIBLE HAPPENED IN: ' + browser.name, error);
+    };
 
-    if (jsonToOutput) {
-      pendingFileWritings++;
+    this.onBrowserComplete = function(browser) {
+        var bro = json[browser.id];
+        // TODO: build suites from partials (= partial results)
+        var browserSuites = json[browser.id].suites;
+        var browserPartials = json[browser.id].partials;
 
-      config.basePath = path.resolve(config.basePath || '.');
-      outputFile = basePathResolve(outputFile);
-      helper.normalizeWinPath(outputFile);
+        // aggregate partial results:
+        _(browserPartials)
+            .groupBy(function(partial) {
+                return partial.suite.join('_suite_sep_') + '__separator__' + partial.title.name;
+            })
+            .map(function(group) {
+                var durations = _(group)
+                    .sortBy(_.property('title.index'))
+                    .map('title.duration')
+                    .value();
 
-      helper.mkdirIfNotExists(path.dirname(outputFile), function() {
-        fs.writeFile(outputFile, JSON.stringify(jsonToOutput), function(err) {
-          if (err) {
-            log.warn('Cannot write JSON report\n\t' + err.message);
-          } else {
-            log.debug('JSON results written to "%s".', outputFile);
-          }
+                var status = {
+                    passes: _.filter(group, function(partial) { return partial.status === 'pass'; }).length,
+                    fails: _.filter(group, function(partial) { return partial.status === 'fail'; }).length,
+                    skips: _.filter(group, function(partial) { return partial.status === 'skip'; }).length
+                };
 
-          if (!--pendingFileWritings) {
-            fileWritingFinished();
-          }
+                var slidingWindowSize = 30,
+                    allowPercentagedDeviation = 4;;
+
+                var lastMeasurements = durations.slice(-slidingWindowSize),
+                    time = average(lastMeasurements),
+                    deviation = 100 * standardDeviation(lastMeasurements) / time;
+
+                return {
+                    suite: group[0].suite,
+                    status: "" + status.passes + " passed, " + status.fails + " failed, " + status.skips + " skipped",
+                    title: {
+                        name: group[0].title.name,
+                        average: time.toFixed(3),
+                        standardDeviation: deviation.toFixed(1),
+                        results: durations
+                    }
+                };
+
+
+            })
+            .forEach(function(suite) {
+              browserSuites.push(suite);
+            });
+
+        // delete partials from benchmark output
+        bro.partials = undefined;
+
+        // add meta data
+        var result = browser.lastResult;
+        bro.test = {
+            tests: result.total + ' tests',
+            errors: (result.disconnected || result.error ? 1 : 0) + ' errors',
+            failed: result.failed + ' failures',
+            skipped: result.skipped + ' skipped',
+            runtime: ((result.netTime || 0) / 1000) + 's'
+        };
+    };
+
+    this.onRunComplete = function(browsers) {
+        var jsonToOutput = json;
+
+        if (jsonToOutput) {
+            pendingFileWritings++;
+
+            config.basePath = path.resolve(config.basePath || '.');
+            outputFile = basePathResolve(outputFile);
+            helper.normalizeWinPath(outputFile);
+
+            helper.mkdirIfNotExists(path.dirname(outputFile), function() {
+                fs.writeFile(outputFile, JSON.stringify(jsonToOutput), function(err) {
+                    if (err) {
+                        log.warn('Cannot write JSON report\n\t' + err.message);
+                    } else {
+                        log.debug('JSON results written to "%s".', outputFile);
+                    }
+
+                    if (!--pendingFileWritings) {
+                        fileWritingFinished();
+                    }
+                });
+            });
+        } else {
+            log.error('JSON report was not created\n\t');
+        }
+    };
+
+    // !!Test case returns!!
+    this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
+        var browserPartials = json[browser.id].partials;
+        console.log('-------------- result.description --------------');
+        console.log(result.description);
+        browserPartials.push({
+            suite: result.suite,
+            status: result.skipped ? 'skip' : (result.success ? 'pass' : 'fail'),
+            title: JSON.parse(result.description)
+            // other fields of result: log, time
         });
-      });
-    } else {
-      log.error('JSON report was not created\n\t');
-    }
-  };
+    };
 
-  // !!Test case returns!!
-  this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
-    var browserSuites = json[browser.id].suites;
-      console.log('-------------- result.description --------------');
-      console.log(result.description);
-    return;
-    browserSuites.push({
-        suite: result.suite,
-        status: result.skipped ? 'skip' : (result.success ? 'pass' : 'fail'),
-        title: JSON.parse(result.description)
-        // other fields of result: log, time
-    });
-  };
-
-  // wait for writing all the json files, before exiting
-  this.onExit = function (done) {
-    if (pendingFileWritings) {
-      fileWritingFinished = done;
-    } else {
-      done();
-    }
-  };
+    // wait for writing all the json files, before exiting
+    this.onExit = function (done) {
+        if (pendingFileWritings) {
+            fileWritingFinished = done;
+        } else {
+            done();
+        }
+    };
 };
 
 JSONReporter.prototype._repeat = function(n, str) {
-  var res = [];
-  var i;
-  for (i = 0; i < n; ++i) {
-    res.push(str);
-  }
-  return res.join('');
+    var res = [];
+    var i;
+    for (i = 0; i < n; ++i) {
+        res.push(str);
+    }
+    return res.join('');
 };
 
 JSONReporter.$inject = ['baseReporterDecorator', 'config', 'emitter', 'logger', 'helper', 'formatError'];
@@ -130,117 +199,117 @@ JSONReporter.$inject = ['baseReporterDecorator', 'config', 'emitter', 'logger', 
 // Generated on Tue Aug 23 2016 14:57:05 GMT+0200 (W. Europe Daylight Time)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: (process.env.TRAVIS ? ['benchmark/tests/travis.config.js'] : []).concat([
-      'benchmark/bundles/*.js'
-    ]),
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha', 'chai'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: (process.env.TRAVIS ? ['benchmark/tests/travis.config.js'] : []).concat([
+            'benchmark/bundles/*.js'
+        ]),
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha', 'json'],
-
-    jsonReporter: {
-      outputFile: 'results/latest.json'
-    },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+        },
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['mocha', 'json'],
+
+        jsonReporter: {
+            outputFile: 'results/latest.json'
+        },
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome_Local'],
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    customLaunchers: {
-      Chrome_Local: {
-        base: 'Chrome',
-        flags: [
-          '--disable-hang-monitor'
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome_Local'],
+
+        customLaunchers: {
+            Chrome_Local: {
+                base: 'Chrome',
+                flags: [
+                    '--disable-hang-monitor'
+                ]
+            },
+            Chrome_Travis_CI: {
+                base: 'ChromeCanary',
+                flags: [
+                    '--no-sandbox',
+                    '--disable-hang-monitor'
+                ]
+            }
+        },
+
+        browserNoActivityTimeout: 3000*60*1000, // 8min (so build does not timeout)
+        browserDisconnectTimeout: 3000*60*1000, // 8min (so build does not timeout)
+        client: {
+            captureConsole: false,
+            mocha: {
+                timeout: 3000 * 60 * 1000
+            }
+        },
+        captureTimeout: 3000 * 60 * 1000, // it was already there
+        browserDisconnectTolerance : 1,
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+
+        plugins: [
+            'karma-chai',
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-chrome-launcher',
+            {'reporter:json': ['type', JSONReporter]}
         ]
-      },
-      Chrome_Travis_CI: {
-        base: 'ChromeCanary',
-        flags: [
-          '--no-sandbox',
-          '--disable-hang-monitor'
-        ]
-      }
-    },
+    });
 
-    browserNoActivityTimeout: 3000*60*1000, // 8min (so build does not timeout)
-    browserDisconnectTimeout: 3000*60*1000, // 8min (so build does not timeout)
-    client: {
-      captureConsole: false,
-      mocha: {
-        timeout: 3000 * 60 * 1000
-      }
-    },
-    captureTimeout: 3000 * 60 * 1000, // it was already there
-    browserDisconnectTolerance : 1,
+    if(process.env.TRAVIS) {
+        config.browsers = [process.env.TRAVIS ? 'Chrome_Travis_CI' : 'Chrome_Local'];
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+        var now = new Date();
+        var nowString = now.toDateString() + ' ' + now.toTimeString().slice(0,8);
+        nowString = nowString
+            .replace(/:/g, '.')
+            .replace(/ /g, '_');
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    plugins: [
-      'karma-chai',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-chrome-launcher',
-      {'reporter:json': ['type', JSONReporter]}
-    ]
-  });
-
-  if(process.env.TRAVIS) {
-    config.browsers = [process.env.TRAVIS ? 'Chrome_Travis_CI' : 'Chrome_Local'];
-
-    var now = new Date();
-    var nowString = now.toDateString() + ' ' + now.toTimeString().slice(0,8);
-    nowString = nowString
-        .replace(/:/g, '.')
-        .replace(/ /g, '_');
-
-    config.jsonReporter.outputFile = 'results/' + nowString + '.json';
-  }
+        config.jsonReporter.outputFile = 'results/' + nowString + '.json';
+    }
 };
